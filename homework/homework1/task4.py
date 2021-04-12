dcba14aea567f829211ec78fc39a6df49be4ce96
Aleksandr <alexplahotnik@gmail.com>
2021-04-12 18:25:40 +0300

    Modify with black

--- homework/homework1/task4.py
+++ homework/homework1/task4.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    """Finding count of how many tuples (i, j, k, l) there are such 
+    """Finding count of how many tuples (i, j, k, l) there are such
     that A[i] + B[j] + C[k] + D[l] is zero"""
     zero_summ = 0
     for i in a:
